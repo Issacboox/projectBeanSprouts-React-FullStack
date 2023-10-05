6f89cc45db470a139a54a83fe6146f596b10d9a1
Issacboox <nurarat0024@gmail.com>
2023-10-05 15:16:50 +0700

    latest

--- bean/src/Auth.tsx
+++ bean/src/Auth.tsx
@@ -2,56 +2,93 @@ import App from "./App.tsx";
 import { FormEvent, useEffect, useState } from "react";
 import { remult } from "remult";
 
-export default function Auth(){
-    const[username, setUsername] = useState("");
-    const[signIn, setSignedIn] = useState(false);
-    useEffect(() =>{
-        fetch("/api/currentUser").then(async result=>{
-            remult.user = await result.json()
-            if (remult.user){
-               setSignedIn(true) 
-            }
-        })
-    },[])
-    if(!signIn){
-        async function doSignIn(e:FormEvent<HTMLFormElement>){
-            e.preventDefault();
-            const result = await fetch("/api/signIn",{
-                method:"POST",
-                headers:{
-                    "Content-Type":"application/json"
-                },
-                body:JSON.stringify({username})
+export default function Auth() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [signIn, setSignedIn] = useState(false);
+    useEffect(() => {
+        fetch("/api/currentUser")
+            .then(async (result) => {
+                if (!result.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                const responseText = await result.text();
+                if (responseText) {
+                    try {
+                        remult.user = JSON.parse(responseText);
+                        setSignedIn(true);
+                    } catch (error) {
+                        console.error("Error parsing JSON:", error);
+                    }
+                } else {
+                    console.error("Empty response from /api/currentUser");
+                }
             })
-            if (result.ok){
-                remult.user = await result.json();
-                setSignedIn(true)
-                setUsername("")
+            .catch((error) => {
+                console.error("Error fetching /api/currentUser:", error);
+            });
+    }, []);
+    
+    
+    if (!signIn) {
+        async function doSignIn(e: FormEvent<HTMLFormElement>) {
+            e.preventDefault();
+            const result = await fetch("/api/signIn", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ username, password }) // Send both username and password
+            })
+            // Inside your doSignIn function
+            if (result.ok) {
+                const responseText = await result.text();
+                if (responseText) {
+                    remult.user = JSON.parse(responseText);
+                    setSignedIn(true);
+                    setUsername("");
+                    setPassword("");
+                } else {
+                    alert("Empty response from the server");
+                }
             } else {
-                alert(await result.json());
+                alert("Failed to sign in");
             }
-        }
 
-     
+        }
         return <>
-        <h1>Sign in</h1>
-        <main>
-            <form className="form-control" onSubmit={(e) => doSignIn(e)}>
-                <input value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Input username"/>
-                <button>Sign In</button>           
-            </form>
-        </main>
+            <h1>Sign in</h1>
+              <main>
+                <form className="form-control" onSubmit={(e) => doSignIn(e)}>
+                <label className="form-label">Username</label>
+                  <input
+                    className="form-control"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    placeholder="Input username"
+                  />
+                  <label className="form-label">Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Input password"
+                  />
+                  <button>Sign In</button>
+                </form>
+              </main>
         </>
     }
-    async function signOut(){
-        await fetch("/api/signOut",{ method:"POST" });
+    async function signOut() {
+        await fetch("/api/signOut", { method: "POST" });
         setSignedIn(false);
         remult.user = undefined;
     }
 
     return <>
-    <App/>
-    Hello {remult.user!.name} 
-    <button className="btn btn-danger" onClick={() => signOut()}>Sign out</button>
-    </> ;
+        <App />
+        Hello {remult.user!.name}
+        <button className="btn btn-danger" onClick={() => signOut()}>Sign out</button>
+    </>;
 }
--- bean/src/server/auth.ts
+++ bean/src/server/auth.ts
@@ -1,21 +1,27 @@
 import express, { Router } from "express";
 import { UserInfo } from "remult";
 
-const validUsers : UserInfo[] = [
-    {id: "1", name:"doctor", roles:["admin"]},
-    {id: "2", name:"Jason"}
+interface CustomUserInfo extends UserInfo {
+    password: string;
+}
+
+const validUsers: CustomUserInfo[] = [
+    { id: "1", name: "doctor", roles: ["admin"], password: "1122" },
+    { id: "2", name: "Jason", roles: [], password: "2234" }
 ];
 
 export const auth = Router();
 auth.use(express.json());
 
-auth.post("/api/signIn",(req,res)=>{
-    const user = validUsers.find(user=>user.name===req.body.username)
-    if(user){
+auth.post("/api/signIn", (req, res) => {
+    const { username, password } = req.body; // Get both username and password from the request body
+    const user = validUsers.find((user) => user.name === username && user.password === password); // Check both username and password
+
+    if (user) {
         req.session!["user"] = user;
         res.json(user);
-    }else{
-        res.status(404).json("Invalid user")
+    } else {
+        res.status(404).json("Invalid username or password");
     }
 });
 
